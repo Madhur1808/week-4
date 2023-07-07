ae9873e6e136501bcc653fb3f53252691df07109
Madhur1808 <77977225+Madhur1808@users.noreply.github.com>
2023-07-07 23:45:36 +0530

    added get request

--- 01-easy-todo-app/src/App.jsx
+++ 01-easy-todo-app/src/App.jsx
@@ -1,27 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+import "./App.css";
+
+let App = function App() {
+  const [todos, setTodos] = useState([]);
+  // fetch all todos from server
+  useEffect(() => {
+    axios.get("http://localhost:3000/todos").then((response) => {
+      setTodos(response.data);
+    });
+  }, []);
+
+  if (!todos) {
+    return <div>No todos present</div>;
+  }
 
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        {todos.map((todo) => {
+          return (
+            <div>
+              <span>title:{todo.title}</span>
+              <span>Description:{todo.description}</span>
+              <button>Delete</button>
+            </div>
+          );
+        })}
+        <div>
+          <input
+            placeholder="title"
+            type="text"
+            title={title}
+            onChange={changehandler}
+          />
+          <input
+            placeholder="description"
+            type="text"
+            description={description}
+            onChange={changehandler}
+          />
+          <button onClick={addtodo}>Add Todo</button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
-}
+// function Todo(props) {
+//   // Add a delete button here so user can delete a TODO.
+//   return <div>{props.title}</div>;
+// }
 
-export default App
+export default App;
